86b0fff2a3f6ecc613cf6bc0f0b41cf4aa5f1438
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-10-17 14:57:40 -0300

    chore: initial config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
